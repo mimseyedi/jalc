e02e1b992cc749807eb60db099d5067900cc7ee7
Mohammad <mim.seyedi@gmail.com>
2024-04-09 13:52:58 +0330

    The initial design was done and the signatures were written.

---
+++ src/jalc/datetime.py
@@ -0,0 +1,17 @@
+"""
+•  ┓
+┓┏┓┃┏
+┃┗┻┗┗ is a package to work with Jalali date and time.
+┛
+
+This file is related to the Jalali date&time.
+With the help of the 'JalDateTime' class, you can work with the date and time
+based on the Jalali calendar and receive data about the date and time in different ways.
+
+
+Github repo: https://github.com/mimseyedi/jalc
+"""
+
+
+class JalDateTime:
+    ...
